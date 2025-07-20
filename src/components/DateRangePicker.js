6fdbf98fb514eb04c9b54e8d870b1005dc0c7c4b
Vishwas Saxena <185520121+weber404@users.noreply.github.com>
2025-07-20 22:03:46 +0530

    Update DateRangePicker.js

--- src/components/DateRangePicker.js
+++ src/components/DateRangePicker.js
@@ -3,29 +3,22 @@ import React from 'react';
 const DateRangePicker = ({ value, onChange }) => {
   return (
     <div className="date-range-picker">
-     <div style={{ marginBottom: '12px' }}>
-      <label style={{ display: 'block' }}>
-        Start Date:
-        <br />
+      <div style={{ marginBottom: '12px', display: 'flex', alignItems: 'center' }}>
+        <label style={{ marginRight: '8px' }}>Start Date:</label>
         <input
-          style={{ display: 'block', marginTop: '4px' }}
           type="date"
           value={value.start ? value.start.split('T')[0] : ''}
           onChange={e => onChange({ ...value, start: e.target.value })}
         />
-      </label> 
-            </div>
-            <div>
-      <label style={{ display: 'block' }}>
-        End Date (optional):
-        <br />
+      </div>
+      <div style={{ marginBottom: '12px', display: 'flex', alignItems: 'center' }}>
+        <label style={{ marginRight: '8px' }}>End Date (optional):</label>
         <input
-          style={{ display: 'block', marginTop: '4px' }}
           type="date"
           value={value.end ? value.end.split('T')[0] : ''}
           onChange={e => onChange({ ...value, end: e.target.value })}
         />
-      </label> </div>
+      </div>
     </div>
   );
 };
